a3b3e1b82176ba2714865262f139dff674ac1bcd
Yusei-0 <acmnhc@gmail.com>
2022-08-06 01:04:46 -0400

    Add to data-service the data about periods of education and import to education-component

--- src/app/education/education/education.component.ts
+++ src/app/education/education/education.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Period } from 'src/app/core/models/period.model';
+import { DataService } from 'src/app/core/services/data.service';
 
 @Component({
   selector: 'app-education',
@@ -10,9 +11,12 @@ export class EducationComponent implements OnInit {
 
   periods : Period[] = []
 
-  constructor() { }
+  constructor(
+    private data: DataService
+  ) { }
 
   ngOnInit(): void {
+    this.periods = this.data.getPeriodsOfEducation;
   }
 
 }
