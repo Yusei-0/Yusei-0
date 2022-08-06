94aa55546e6c9a15d332915922b5db6c8e1fac34
Yusei-0 <acmnhc@gmail.com>
2022-08-06 09:07:19 -0400

    Fix home styles and eductaion-model importation

--- src/app/education/education.module.ts
+++ src/app/education/education.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EducationComponent } from './education/education.component';
 import { EducationRoutingModule } from './education-routing.module';
-import { PeriodComponent } from './components/period/period.component';
 import { SharedModule } from '../shared/shared.module';
+import { PeriodComponent } from './components/period/period.component';
 
 
 
